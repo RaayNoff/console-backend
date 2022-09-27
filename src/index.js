d890c2e673990076d3354a52dfa68850758ff2ad
RaayNoff <kraxtv471365@gmail.com>
2022-09-27 19:59:55 +0700

    Changed fill color of svgs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const skillsRouter = require("./routes/skills.router");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(cors({}));
 app.use(express.json());
 app.use("/api", skillsRouter);
 app.use(express.static("src/static"));
