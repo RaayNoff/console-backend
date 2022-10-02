dc2a01ce05744b5c5a3bc48395cadb78a081233a
RaayNoff <kraxtv471365@gmail.com>
2022-10-03 01:44:30 +0700

    Changed db settings

--- src/data/db.js
+++ src/data/db.js
@@ -1,8 +1,7 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  connectionString:
-    "postgres://rlyfhepgggygbh:290d646387025db9e0bb48c2e360b2bc05ba0ddb7f9ab4d1778e0c9c80c865da@ec2-44-208-88-195.compute-1.amazonaws.com:5432/d1prmtkg2prdv9",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
