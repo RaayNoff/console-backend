14d280c9f422e3bdd1df18cca654b0f338cf27e2
RaayNoff <kraxtv471365@gmail.com>
2022-10-03 01:01:13 +0700

    Changed db connection

--- src/data/db.js
+++ src/data/db.js
@@ -1,12 +1,15 @@
-const Pool = require("pg").Pool;
+const { Client } = require("pg");
 
-const pool = new Pool({
+const client = new Client({
   user: "rlyfhepgggygbh",
   password: "290d646387025db9e0bb48c2e360b2bc05ba0ddb7f9ab4d1778e0c9c80c865da",
   host: "ec2-44-208-88-195.compute-1.amazonaws.com",
   port: 5432,
   database: "d1prmtkg2prdv9",
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+client.connect();
 
-module.exports = pool;
+module.exports = client;
