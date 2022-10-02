205ccdfebba22852a58de5cb336dade57fca1299
RaayNoff <kraxtv471365@gmail.com>
2022-10-03 00:27:30 +0700

    Updated db data

--- src/data/db.js
+++ src/data/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "root",
-  host: "localhost",
+  user: "rlyfhepgggygbh",
+  password: "290d646387025db9e0bb48c2e360b2bc05ba0ddb7f9ab4d1778e0c9c80c865da",
+  host: "ec2-44-208-88-195.compute-1.amazonaws.com",
   port: 5432,
-  database: "console",
+  database: "d1prmtkg2prdv9",
 });
 
 module.exports = pool;
